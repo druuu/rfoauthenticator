baac6e87d1c2ee7c2995b3ae0a6626a6270f8676
dinesh <dinesh@micropyramid.com>
2019-04-17 10:49:00 +0530

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='rfoauthenticator',
+      version='0.1',
+      description='oauthenticator custom auth0 authentication',
+      author='refactored',
+      author_email='info@refactored.ai',
+      install_requires=['oauthenticator'],
+      packages=['rfoauthenticator'],
+      zip_safe=False)
