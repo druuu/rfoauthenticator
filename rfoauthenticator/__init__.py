be075c3fce8f15c2d10110ddd2d2cc86b98c572d
dinesh <dinesh@micropyramid.com>
2019-07-05 15:21:21 +0530

    secure call using servicetoken

--- rfoauthenticator/__init__.py
+++ rfoauthenticator/__init__.py
@@ -7,8 +7,10 @@ from tornado.httpclient import HTTPRequest, AsyncHTTPClient
 from oauthenticator.auth0 import Auth0OAuthenticator
 
 
-AUTH0_SUBDOMAIN = os.getenv('AUTH0_SUBDOMAIN')
-REFACTORED_ACCOUNTS_DOMAIN = os.getenv('REFACTORED_ACCOUNTS_DOMAIN')
+AUTH0_SUBDOMAIN = os.getenv("AUTH0_SUBDOMAIN")
+REFACTORED_ACCOUNTS_DOMAIN = os.getenv("REFACTORED_ACCOUNTS_DOMAIN")
+SERVICE_TOKEN = os.getenv("SERVICE_TOKEN")
+
 
 class RFAuth0OAuthenticator(Auth0OAuthenticator):
     @gen.coroutine
@@ -16,46 +18,50 @@ class RFAuth0OAuthenticator(Auth0OAuthenticator):
         code = handler.get_argument("code")
         http_client = AsyncHTTPClient()
         params = {
-            'grant_type': 'authorization_code',
-            'client_id': self.client_id,
-            'client_secret': self.client_secret,
-            'code':code,
-            'redirect_uri': self.get_callback_url(handler)
+            "grant_type": "authorization_code",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "code": code,
+            "redirect_uri": self.get_callback_url(handler),
         }
         url = "https://%s.auth0.com/oauth/token" % AUTH0_SUBDOMAIN
 
-        req = HTTPRequest(url,
-                          method="POST",
-                          headers={"Content-Type": "application/json"},
-                          body=json.dumps(params),
-                          connect_timeout=60.0,
-                          request_timeout=60.0,
-                          )
-        
+        req = HTTPRequest(
+            url,
+            method="POST",
+            headers={"Content-Type": "application/json"},
+            body=json.dumps(params),
+            connect_timeout=60.0,
+            request_timeout=60.0,
+        )
+
         resp = yield http_client.fetch(req)
-        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
-        
-        access_token = resp_json['access_token']
-        
+        resp_json = json.loads(resp.body.decode("utf8", "replace"))
+
+        access_token = resp_json["access_token"]
+
         # Determine who the logged in user is
-        headers={"Accept": "application/json",
-                 "User-Agent": "JupyterHub",
-                 "Authorization": "Bearer {}".format(access_token)
+        headers = {
+            "Accept": "application/json",
+            "User-Agent": "JupyterHub",
+            "Authorization": "Bearer {}".format(access_token),
         }
-        req = HTTPRequest("https://%s.auth0.com/userinfo" % AUTH0_SUBDOMAIN,
-                          method="GET",
-                          headers=headers,
-                          connect_timeout=60.0,
-                          request_timeout=60.0,
-                          )
+        req = HTTPRequest(
+            "https://%s.auth0.com/userinfo" % AUTH0_SUBDOMAIN,
+            method="GET",
+            headers=headers,
+            connect_timeout=60.0,
+            request_timeout=60.0,
+        )
         resp = yield http_client.fetch(req)
-        resp_json = json.loads(resp.body.decode('utf8', 'replace'))
+        resp_json = json.loads(resp.body.decode("utf8", "replace"))
 
         # set_user
-        url = "https://%s/user/create" % REFACTORED_ACCOUNTS_DOMAIN
-        d = {"email": resp_json['email']}
+        url = "https://%s/user/create?servicetoken=%s" % (
+            REFACTORED_ACCOUNTS_DOMAIN,
+            SERVICE_TOKEN,
+        )
+        d = {"email": resp_json["email"]}
         r = requests.post(url, data=d)
         d = r.json()
-        return {
-            'name': d['username']
-        }
+        return {"name": d["username"]}
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='rfoauthenticator',
-      version='1.7',
-      description='oauthenticator custom auth0 authentication',
-      author='refactored',
-      author_email='info@refactored.ai',
-      install_requires=['oauthenticator', 'requests'],
-      packages=['rfoauthenticator'],
-      zip_safe=False)
+setup(
+    name="rfoauthenticator",
+    version="1.8",
+    description="oauthenticator custom auth0 authentication",
+    author="refactored",
+    author_email="info@refactored.ai",
+    install_requires=["oauthenticator", "requests"],
+    packages=["rfoauthenticator"],
+    zip_safe=False,
+)
